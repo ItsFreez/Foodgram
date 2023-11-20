1898650065e1aa108fef870f330e03fd02bf8eac
Игорь Митяшин <ItsFreez@yandex.ru>
2023-11-20 17:35:09 +0300

    Add admin settings for models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Favorite, Ingredient, Recipe, Tag, ShoppingCart
+
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name', 'slug',)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'pub_date',)
+    search_fields = ('name',)
+    list_filter = ('tags', 'author',)
+    filter_horizontal = ('tags',)
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+    list_filter = ('recipe',)
+    search_fields = ('user',)
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+    list_filter = ('recipe',)
+    search_fields = ('user',)
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Follow, User
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    search_fields = ('username', 'email',)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    list_filter = ('following',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Follow, FollowAdmin)
