33e6d6b55b1a7cf24799412e4ca1f9408bb55136
Игорь Митяшин <ItsFreez@yandex.ru>
2023-11-22 11:27:33 +0300

    Remove filter for ingredients in admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -13,7 +13,6 @@ class TagAdmin(admin.ModelAdmin):
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit',)
-    list_filter = ('name',)
     search_fields = ('name',)
 
 
