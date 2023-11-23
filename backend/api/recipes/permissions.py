be82c84b242bc89f8ef9d5511d3c4482d3ded001
ItsFreez <132353894+ItsFreez@users.noreply.github.com>
2023-11-23 11:36:36 +0300

    Update permissions.py

--- backend/api/recipes/permissions.py
+++ backend/api/recipes/permissions.py
@@ -6,7 +6,7 @@ class IsAdminOrOwnerOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
-                or (request.user.is_authenticated and request.user.is_staff))
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
